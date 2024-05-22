016918269af3a52b04d702239f3e8d350bd5564d
EYoung21 <eliyoung4now@email.com>
2024-05-22 16:09:15 -0400

    added up to pt 6

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,11 +1,15 @@
 extends CharacterBody2D
 
 const ACCELERATION = 500
-const MAX_SPEED = 80
+const MAX_SPEED = 50
 const FRICTION = 500
 
 #var velocity = Vector2.ZERO
 #ALREADY DEFINED?
+
+@onready var animationPlayer = $AnimationPlayer
+@onready var animationTree = $AnimationTree
+@onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -15,9 +19,13 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 		print(velocity)
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	move_and_slide()
